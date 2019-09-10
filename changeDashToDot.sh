09e310dc420ba1bbe16258ffc19ea7a3160c0dac
yge <yge@it-economics.de>
2019-09-10 12:29:07 +0200

    logic wrong direction

--- changeDashToDot.sh
+++ changeDashToDot.sh
@@ -4,7 +4,7 @@ echo "before: "
 cat latest.yml
 
 echo "change dot to dash"
-sed -i '' 's/electron-boilerplate.setup./electron-boilerplate-setup-/g' latest.yml
+sed -i '' 's/electron-boilerplate-setup-/electron-boilerplate.setup./g' latest.yml
 
 echo "after: "
 cat latest.yml
